b580a9a7e91980cb741df41bd790ac9e9a3e4260
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2026-03-10 01:07:26 +0200

    Refactor footer social media links for improved structure
    - Wrapped the social media links in a navigation element for better semantic structure and accessibility.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,13 +33,15 @@
 				<a href="/" class="site-footer__logo" aria-label="Digitalbank home">
 					<img src="/images/logo-light.svg" alt="" width="139" height="20" />
 				</a>
-				<ul class="site-footer__social" aria-label="Social media">
-					<li><a href="#" aria-label="Facebook"><img src="/images/icon-facebook.svg" alt="" width="20" height="20" /></a></li>
-					<li><a href="#" aria-label="YouTube"><img src="/images/icon-youtube.svg" alt="" width="20" height="20" /></a></li>
-					<li><a href="#" aria-label="Twitter"><img src="/images/icon-twitter.svg" alt="" width="20" height="20" /></a></li>
-					<li><a href="#" aria-label="Pinterest"><img src="/images/icon-pinterest.svg" alt="" width="20" height="20" /></a></li>
-					<li><a href="#" aria-label="Instagram"><img src="/images/icon-instagram.svg" alt="" width="20" height="20" /></a></li>
-				</ul>
+				<nav class="site-footer__social-wrap" aria-label="Social media">
+					<ul class="site-footer__social">
+						<li><a href="#" aria-label="Facebook"><img src="/images/icon-facebook.svg" alt="" width="20" height="20" /></a></li>
+						<li><a href="#" aria-label="YouTube"><img src="/images/icon-youtube.svg" alt="" width="20" height="20" /></a></li>
+						<li><a href="#" aria-label="Twitter"><img src="/images/icon-twitter.svg" alt="" width="20" height="20" /></a></li>
+						<li><a href="#" aria-label="Pinterest"><img src="/images/icon-pinterest.svg" alt="" width="20" height="20" /></a></li>
+						<li><a href="#" aria-label="Instagram"><img src="/images/icon-instagram.svg" alt="" width="20" height="20" /></a></li>
+					</ul>
+				</nav>
 			</div>
 			<nav class="site-footer__nav" aria-label="Footer">
 				<ul class="site-footer__links site-footer__links--col1">
